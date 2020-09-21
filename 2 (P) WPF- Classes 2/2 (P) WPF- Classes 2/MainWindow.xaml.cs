6cd539edf7bf061467ebb450d42c424249310721
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-09-21 12:39:35 -0500

    Finished

---
+++ 2 (P) WPF- Classes 2/2 (P) WPF- Classes 2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2__P__WPF__Classes_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            Toy Toy1 = new Toy();
+            if (string.IsNullOrEmpty(txtManufacturer.Text) || (string.IsNullOrEmpty(txtName.Text)))
+            {
+                MessageBox.Show("Manufacturer or Name was null or empty, please try again");
+            }
+            else
+            {
+                Toy1.Manufacturer = txtManufacturer.Text;
+                Toy1.Name = txtName.Text;
+                
+            }
+            if (!int.TryParse(txtPrice.Text, out var value))
+            {
+                MessageBox.Show("Price must be a whole number");
+            }
+            else
+            {
+                Toy1.Price = Convert.ToDouble(txtPrice.Text);
+                
+            }
+            ListBox.Items.Add(Toy1);
+
+            txtName.Text = "";
+            txtManufacturer.Text = "";
+            txtPrice.Text = "";
+
+        }
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)ListBox.SelectedItem;
+            MessageBox.Show($"{selectedToy.GetAisle()}");
+        }
+    }
+}
---
+++ 2 (P) WPF- Classes 2/2 (P) WPF- Classes 2/Toy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2__P__WPF__Classes_2
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+
+        }
+        public Toy(string name, string manufacturer, double price)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Price = Convert.ToDouble(price);
+
+        }
+        public override string ToString()
+        {
+            string output = $"{Manufacturer}- {Name}";
+            return output;
+        }
+        public string GetAisle()
+        {
+            return $"{Manufacturer[0]}: - {Price}";
+        }
+    }
+}
