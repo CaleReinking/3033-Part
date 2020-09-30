af07863bdec93d805d107422a1e1a8b7128c1cbc
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-09-30 01:44:15 -0500

    Killing it

---
+++ (P/(P/Characters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _P
+{
+    class Characters
+    {
+        public Funny funny { get; set; }
+        public List<Funny> results { get; set; }
+    }
+
+    public class Funny
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+    }
+
+}
---
+++ (P/(P/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string webService = @"https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2000";
+
+            //HttpClient client = new HttpClient();
+            Characters characters;
+            using (var client = new HttpClient())
+            {
+                var results = client.GetStringAsync(webService).Result;
+
+                characters = JsonConvert.DeserializeObject<Characters>(results);
+            }
+
+            foreach (var character in characters.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+        }
+    }
+}
