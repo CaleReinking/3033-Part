d2cd424ec8e09ad518152ea10bacb7f1aed3fa37
Cale Reinking <Cale.J.Reinking-1@ou.edu>
2020-10-13 23:22:15 -0500

    ran it

---
+++ WpfApp1/WpfApp1/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+        public Student()
+           {
+            return ;
+           }
+        public Student(string firstName, string lastName, string major, double gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Major = major;
+            GPA = gpa;
+        }
+        public string CalculateDistinction(double Gpa)
+        {
+            string Distinction = "";
+            if (Gpa>=4.0)
+            {
+
+            }
+            return Distinction;
+        }
+    }
+    class Address
+    {
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+        public Address()
+        {
+
+        }
+        public Address(int streetnumber, string streetname, string state, string city, int zipcode)
+        {
+            StreetNumber = streetnumber;
+            StreetName = streetname;
+            State = state;
+            City = city;
+            Zipcode = zipcode;
+
+            
+        }
+        public override string ToString()
+        {
+            string full = "";
+            full = $"{StreetNumber}";
+            return full;
+        }
+    }
+}
